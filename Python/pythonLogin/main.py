53047b2343352d5ebe1c46dd752f897091912ec4
Oscar Sarabia <ferarersunl@hotmail.com>
2023-03-14 20:26:09 -0600

    latest hw

--- Python/pythonLogin/main.py
+++ Python/pythonLogin/main.py
@@ -7,10 +7,15 @@ ids_existentes = []
 
 
 # Leer el archivo de texto y agregar los IDs existentes a la lista
-with open("credenciales.txt", "r") as file:
-    for line in file:
-        fields = line.strip().split(":")
-        ids_existentes.append(int(fields[0]))
+try:
+    with open("credenciales.txt", "r") as file:
+        for line in file:
+            fields = line.strip().split(":")
+            ids_existentes.append(int(fields[0]))
+except FileNotFoundError:
+        print("\n")
+        print("El archivo no se encontró")
+        input("Presiona enter para continuar. . .")
 
 
 # Definir una función para crear una nueva cuenta
@@ -20,6 +25,8 @@ def crear_cuenta():
     # Solicitar al usuario que ingrese un nombre de usuario y contraseña
     username = input("Ingrese su nombre de usuario: ")
     password = input("Ingrese su contraseña (debe tener al menos 8 caracteres y símbolos especiales): ")
+    security_question = input("Escribe una pregunta de seguridad (p. ej., ¿Cuál es el nombre de tu mascota?): ")
+    security_answer = input("Escribe la respuesta a la pregunta de seguridad: ")
 
     # Validar la contraseña
     if len(password) != 8 or not re.search("[!@#$%^&*()_+-=]", password):
@@ -33,16 +40,15 @@ def crear_cuenta():
     while id_cuenta in ids_existentes:
         id_cuenta += 1
 
-    # Guardar las credenciales de inicio de sesión y el ID de la cuenta en el archivo de texto
+    # Guardar las credenciales de inicio de sesión, la pregunta de seguridad y su respuesta, y el ID de la cuenta en el archivo de texto
     with open("credenciales.txt", "a") as file:
-        file.write(f"{id_cuenta}:{username}:{password}\n")
+        file.write(f"{id_cuenta}:{username}:{password}:{security_question}:{security_answer}\n")
     print(f"¡Cuenta creada con éxito! Su nombre de usuario es {username} y su ID de cuenta es {id_cuenta}")
     print('----------------------------')
     input('Presiona enter para continuar. . . ')
     
     # Agregar el nuevo ID a la lista de IDs existentes
     ids_existentes.append(id_cuenta)
-
 
 # Inicia sesión en una cuenta existente
 def inicio_sesion():
@@ -62,6 +68,28 @@ def inicio_sesion():
                     input('Presiona enter para acceder. . . ')
                     return
         print("Nombre de usuario o contraseña no válidos")
+    except FileNotFoundError:
+        print("El archivo no se encontró")
+        input("Presiona enter para continuar. . .")
+
+def recuperar_contrasena():
+    # Preguntar por el username y validar si existe en el registro
+    username = input("Ingrese su nombre de usuario: ")
+    security_question = input("Escribe una pregunta de seguridad (p. ej., ¿Cuál es el nombre de tu mascota?): ")
+    security_answer = input("Escribe la respuesta a la pregunta de seguridad: ")
+    try:
+        with open("credenciales.txt", "r") as file:
+            animacion()
+            for line in file:
+                fields = line.strip().split(":")
+                if fields[1] == username and fields[3] == security_question and fields[4] == security_answer:
+                    print(f"¡Inicio de sesión exitoso! Bienvenido, {username}")
+                    newPassword = input("¿Cual será tu nueva contraseña?: ")
+                    fields[2] = newPassword
+                    print('----------------------------')
+                    input('Presiona enter para acceder. . . ')
+                    return
+        print("Nombre de usuario, pregunta o contraseña erroneas.")
     except FileNotFoundError:
         print("El archivo no se encontró")
         input("Presiona enter para continuar. . .")
@@ -114,7 +142,8 @@ while True:
     print("¿Qué te gustaría hacer?")
     print("1. Crear una nueva cuenta")
     print("2. Iniciar sesión en una cuenta existente")
-    print("3. Salir")
+    print("3. Olvide mi contraseña")
+    print("4. Salir")
     choice = input()
 
     # Menú
@@ -125,6 +154,9 @@ while True:
         limpiar()
         inicio_sesion()
     elif choice == "3":
+        recuperar_contrasena()
+        limpiar()
+    elif choice == "4":
         limpiar()
         salir()
         break
